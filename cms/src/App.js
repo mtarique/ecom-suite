d85e77503c3275c3cc4ae0c09c59297a09f7ba9a
mt-chl <tarique@constructionhelpline.com>
2024-12-05 19:23:48 +0530

    CRM folder renamed to CMS

---
+++ cms/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <h1>Welcome to MyApp</h1>
+      <p>This is a simple template with a header, footer, and layout using Bootstrap 5.</p>
+    </Layout>
+  );
+}
+
+export default App;
